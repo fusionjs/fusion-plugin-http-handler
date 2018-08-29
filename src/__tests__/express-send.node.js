994c9cb929d2c3e914700aab8b46345185857dcf
Giancarlo Anemone <ganemone@uber.com>
2018-08-29 11:54:07 -0400

    Listen on end event rather than override ctx.res methods

---
+++ src/__tests__/express-send.node.js
@@ -0,0 +1,45 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import test from 'tape-cup';
+import App from 'fusion-core';
+import express from 'express';
+import {HttpHandlerToken} from '../tokens.js';
+import HttpHandlerPlugin from '../server.js';
+import {startServer} from '../test-util.js';
+
+test('http handler with express using send', async t => {
+  const app = new App('test', () => 'test');
+  app.register(HttpHandlerPlugin);
+  const expressApp = express();
+  expressApp.get('/express', (req, res) => {
+    res.send('OK');
+  });
+  app.register(HttpHandlerToken, expressApp);
+  app.middleware((ctx, next) => {
+    ctx.req.secure = false;
+    ctx.body = 'hit fallthrough';
+    return next();
+  });
+
+  const {server, request} = await startServer(app.callback());
+
+  t.equal(await request('/express'), 'OK', 'express routes can send responses');
+  t.equal(
+    await request('/fallthrough'),
+    'hit fallthrough',
+    'express routes can delegate back to koa'
+  );
+  t.equal(
+    await request('/fallthrough'),
+    'hit fallthrough',
+    'express routes can delegate back to koa'
+  );
+  server.close();
+  t.end();
+});
--- src/server.js
+++ src/server.js
@@ -9,10 +9,8 @@
 /* eslint-env node */
 
 import {createPlugin} from 'fusion-core';
-import {HttpHandlerToken} from './tokens.js';
-
 import type {FusionPlugin} from 'fusion-core';
-
+import {HttpHandlerToken} from './tokens.js';
 import type {DepsType, ServiceType} from './types.js';
 
 const plugin =
@@ -29,19 +27,20 @@ const plugin =
           return next();
         }
         return new Promise((resolve, reject) => {
-          const oldEnd = ctx.res.end.bind(ctx.res);
-          // $FlowFixMe
-          ctx.res.end = (data, encoding, cb) => {
+          const {req, res} = ctx;
+          const listener = () => {
             ctx.respond = false;
-            return next()
-              .then(resolve)
-              .then(() => {
-                oldEnd(data, encoding, cb);
-              });
+            return next().then(resolve);
           };
-          handler(ctx.req, ctx.res, () => {
-            // $FlowFixMe
-            ctx.res.end = oldEnd;
+          res.on('end', listener);
+          handler(req, res, () => {
+            // Express mutates the req object to make this property non-writable.
+            // We need to make it writable because other plugins (like koa-helmet) will set it
+            Object.defineProperty(req, 'secure', {
+              value: req.secure,
+              writable: true,
+            });
+            res.removeListener('end', listener);
             return next()
               .then(resolve)
               .catch(reject);
